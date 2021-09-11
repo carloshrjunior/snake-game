a085ab6a5d9b67cc906f35c13e9c1e2b0811d055
Carlos <carlosh.rjunior@gmail.com>
2021-09-11 12:36:50 -0300

    Adicionando Game Over

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,13 @@ function startGame() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(let i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over :(');
+        }
+    }
+
     createBg();
     createSnake();
     drawFood();
@@ -62,8 +69,6 @@ function startGame() {
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-    // snake.pop();
 
     let newHead = {
         x: snakeX,
